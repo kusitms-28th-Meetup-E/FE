e9dcce3067aa0427c84f4f47ebeef92a19b01aee
chamny20 <chamny20@naver.com>
2023-11-16 03:38:31 +0900

    feat : add profile big version (#37)

--- src/components/atoms/profile/index.tsx
+++ src/components/atoms/profile/index.tsx
@@ -1,7 +1,7 @@
 import temp from "@/assets/react.svg";
 import { ProfileProps } from "@/types";
 
-import { UserInfo } from "./style";
+import { BigUserInfo, UserInfo } from "./style";
 
 export const Profile = ({ nickname, profileImg, date }: ProfileProps) => {
   profileImg = temp;
@@ -17,5 +17,22 @@ export const Profile = ({ nickname, profileImg, date }: ProfileProps) => {
         <p className="content-date">{date}</p>
       </div>
     </UserInfo>
+  );
+};
+
+export const BigProfile = ({ nickname, profileImg, date }: ProfileProps) => {
+  profileImg = temp;
+
+  return (
+    <BigUserInfo>
+      <img
+        src={profileImg}
+        alt=""
+      />
+      <div>
+        <p className="user-nickname">{nickname}</p>
+        <p className="content-date">{date}</p>
+      </div>
+    </BigUserInfo>
   );
 };
