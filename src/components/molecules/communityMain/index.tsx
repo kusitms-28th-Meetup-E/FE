860a1d22bb5e86a270b68d553cd696554983da17
chamny20 <chamny20@naver.com>
2023-11-16 15:13:15 +0900

    feat : apply dummy-data (#37)

--- src/components/molecules/communityMain/index.tsx
+++ src/components/molecules/communityMain/index.tsx
@@ -1,30 +1,23 @@
 import { KeywordTag, TopicTag } from "@/components/atoms/tag";
+import { CommunityPostProps } from "@/types";
 
 import { CommunityMainContainer } from "./style";
 
-export const CommunityMain = () => {
+export const CommunityMain = ({ data }: { data: CommunityPostProps }) => {
   return (
     <CommunityMainContainer>
       <div className="top-wrapper">
         <div className="content-category">
-          <TopicTag category="환경" />
-          <KeywordTag category="환경" />
-          <KeywordTag category="환경" />
+          <TopicTag category={data.area} />
+          <KeywordTag category={data.topic} />
+          <KeywordTag category={data.keyword} />
         </div>
-        <div className="content-text">
-          일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는
-          비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며,
-          강제성이 있는 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로
-          기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기...적 기록이다. 보통 일일 단위로
-          기...적 기록이다. 보통 일일 단위로 기 기록이다. 보통 일일 단위로 기록하지만, 작성하는
-          개인에 따라 천차만별이며, 강제성이 있는 개인이 일상에서 체험하는 경험, 생각, 감상 등의
-          제반사항을 하루 단위로 기록하는 비공식적
-        </div>
+        <div className="content-text">{data.communityText}</div>
       </div>
       <div className="content-quot">
         <div className="quot-text">
           <p>인용한 콘텐츠</p>
-          <div>서이초 사건'에 분노한 교사들…광화문 메운 '검은 물결' | 서울신문</div>
+          <div>{data.quotText}</div>
         </div>
         <img
           src="https://images.chosun.com/resizer/XKL6ePOdAuAn81yF-ZBOY8VyQWs=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/JMI3RCEB2Y7QSIUWJKT2MT7CC4.jpg"
--- src/components/organisms/SubjectDetail/CommunityMainList.tsx
+++ src/components/organisms/SubjectDetail/CommunityMainList.tsx
@@ -2,19 +2,33 @@ import styled from "styled-components";
 
 import { BigProfile } from "@/components/atoms/profile";
 import { CommunityMain } from "@/components/molecules/communityMain";
+import { mainCommunityData } from "@/dummy/mainCommunityData";
 
 export const CommunityMainList = () => {
   return (
-    <CommunityMainListContainer>
-      <BigProfile
-        nickname="chaemin"
-        date="2001.04.30"
-        profileImg=""
-      />
-      <CommunityMain />
-    </CommunityMainListContainer>
+    <FirstWrapper>
+      {mainCommunityData.map((item, idx) => {
+        return (
+          <CommunityMainListContainer key={idx}>
+            <BigProfile
+              nickname={item.nickname}
+              date={item.date}
+              profileImg={item.profileImg}
+            />
+            <CommunityMain data={item} />
+          </CommunityMainListContainer>
+        );
+      })}
+    </FirstWrapper>
   );
 };
+
+export const FirstWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  background-color: #eee;
+`;
 
 export const CommunityMainListContainer = styled.div`
   background-color: #eee;
