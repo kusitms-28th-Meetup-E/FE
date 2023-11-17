b7dcbdda5b3eb7a6e8fbab61ad3dbb16cdbbcc3f
chamny20 <chamny20@naver.com>
2023-11-17 16:42:21 +0900

    feat : apply category-bar state (#37)

--- src/components/organisms/SubjectDetail/CommunityMainList.tsx
+++ src/components/organisms/SubjectDetail/CommunityMainList.tsx
@@ -1,13 +1,53 @@
+import { useEffect, useState } from "react";
+
 import styled from "styled-components";
 
+// import { getCommunityAllData } from "@/apis";
 import { BigProfile } from "@/components/atoms/profile";
 import { CommunityMain } from "@/components/molecules/communityMain";
-import { mainCommunityData } from "@/dummy/mainCommunityData";
+// import { CommunityMain } from "@/components/molecules/communityMain";
+import { EduData, envirData } from "@/dummy/AreaData";
+import { CommunityItemProps } from "@/types";
 
-export const CommunityMainList = () => {
+export const CommunityMainList = ({ selectedTab }: { selectedTab: number }) => {
+  const [tabData, setTabData] = useState<CommunityItemProps[]>([]);
+
+  useEffect(() => {
+    let dataFetch: CommunityItemProps[] = [];
+
+    switch (selectedTab) {
+      case 0: // 전체 api 갖고와서 데이터 넣어주기
+        // getCommunityAllData()
+        //   .then((res) => {
+        //     console.log(res.data);
+        //     dataFetch = res.data;
+        //   })
+        //   .catch((err) => console.log(err));
+        dataFetch = envirData;
+
+        break;
+      case 1: // 일자리 · 노동
+        dataFetch = envirData; //일단 환경 더미 데이터만 넣어놨음
+        break;
+      case 2: // 주거 · 사회 안전망
+        dataFetch = envirData;
+        break;
+      case 3: // 환경
+        dataFetch = envirData;
+        break;
+      case 4: // 교육
+        dataFetch = EduData;
+        break;
+      default:
+        break;
+    }
+
+    setTabData(dataFetch);
+  }, [selectedTab]);
+
   return (
     <FirstWrapper>
-      {mainCommunityData.map((item, idx) => {
+      {tabData.map((item, idx) => {
         return (
           <CommunityMainListContainer key={idx}>
             <BigProfile
@@ -28,6 +68,9 @@ export const FirstWrapper = styled.div`
   flex-direction: column;
   gap: 30px;
   background-color: #eee;
+  /* width: 690px; */
+  width: 63.8%; // 690/1080 비율
+  margin: 0 auto;
 `;
 
 export const CommunityMainListContainer = styled.div`
--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
+import { CategoryBar } from "@/components/molecules/categoryBar";
+import { CommunityPost } from "@/components/molecules/communityPost";
 import { CommunityMainList } from "@/components/organisms/SubjectDetail/CommunityMainList";
+import { envirData } from "@/dummy/AreaData";
 
 export const CommunityPage = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabChange = (idx: number) => {
+    setSelectedTab(idx);
+  };
+
   return (
     <>
-      <CommunityMainList />
+      <CategoryBar onSelectTab={handleTabChange} />
+      <CommunityMainList selectedTab={selectedTab} />
+      <br />
+      {envirData.map((item, idx) => (
+        <CommunityPost
+          key={idx}
+          data={item}
+        />
+      ))}
     </>
   );
 };
